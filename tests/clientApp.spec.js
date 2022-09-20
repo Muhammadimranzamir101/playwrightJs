2cba5f22d9238128010fb3a3ba908b0d4d534382
Muhammad Imran <muhammadimranzamir786@gmail.com>
2022-09-20 17:28:34 +0500

    modifying waitForNavigation with promise.all

--- tests/clientApp.spec.js
+++ tests/clientApp.spec.js
@@ -11,9 +11,12 @@ test('clientApp login', async ({page})=>{
 
     await id.type('anshika@gmail.com')
     await pwd.fill('Iamking@000')
-    await loginBtn.click()
+
+    await Promise.all([
+        page.waitForNavigation(),
+        loginBtn.click()
+    ])
      //await page.waitForLoadState('networkidle') // Not Working
-    await page.waitForNavigation()
     const titles = await cardTitle.allTextContents()
     console.log(titles)
 })
