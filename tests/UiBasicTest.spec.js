2e4289080c2483add8eaad5d83c95cba54b2710d
Muhammad Imran <muhammadimranzamir786@gmail.com>
2022-09-20 16:37:42 +0500

    get textContent of single and multiple elements

--- tests/UiBasicTest.spec.js
+++ tests/UiBasicTest.spec.js
@@ -26,6 +26,7 @@ test.only('Login and validation',async({page})=>{
     const userName = page.locator('#username');
     const password = page.locator('#password');
     const signInBtn = page.locator('//input[@id="signInBtn"]')
+    const cardTitle = page.locator('.card-title a')
 
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
 
@@ -34,7 +35,12 @@ test.only('Login and validation',async({page})=>{
     await userName.type('rahulshettyacademy')
     await password.fill('learning')
     await signInBtn.click()
-    console.log(await page.locator('app-card-list[class="row"]>app-card:nth-child(1) h4').textContent())
+    // console.log(await page.locator('app-card-list[class="row"]>app-card:nth-child(1) h4').textContent())
+    console.log(await cardTitle.first().textContent())
+    console.log(await cardTitle.nth(1).textContent())
+    console.log(await cardTitle.last().textContent()) // returns single element.
+    const allTitles = await cardTitle.allTextContents(); // could not get total elements because list (array) can return zero elements.
+    console.log(allTitles);
 
 })
 
@@ -46,7 +52,6 @@ test('Polling on playwright', async ({page})=>{
         //return jsonBody.id;
         return response.status();
       }, {
-        intervals: [1000, 2000, 10000],
         message: 'Try Again',
         timeout: 10000,
       }).toBe(200);
