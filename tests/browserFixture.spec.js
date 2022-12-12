5138d71d55404b0d9f4999ab421ba87b807f4d9e
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2022-12-12 16:04:08 +0500

    Add test case to use browser fixture

---
+++ tests/browserFixture.spec.js
@@ -0,0 +1,14 @@
+const {test, expect} = require('@playwright/test');
+
+//Fixture is a global variable that is available to every test case in the project.
+
+test('Browser fixture Playwright test',async ({browser})=>{
+
+    const context = await browser.newContext(); //opens a browser with no cookies and context (Fresh instance of browser)
+    const page = await context.newPage(); // add a tab to opened browser
+
+    await page.goto('https://www.daraz.pk/')
+    await this.page.waitForLoadState('load');
+    await this.page.locator('button[class="airship-btn airship-btn-deny"]').click();
+    
+})
