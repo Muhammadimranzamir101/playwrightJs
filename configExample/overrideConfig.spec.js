d7a95703146eeab246816636976d6b90d4ef98c7
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2022-12-12 17:05:46 +0500

    add test case to override the config

---
+++ configExample/overrideConfig.spec.js
@@ -0,0 +1,10 @@
+const { test, expect } = require('@playwright/test');
+
+
+test.use({ viewport: { width: 600, height: 900 }, browserName: 'firefox' }); //Override config in test file
+
+test('Page fixture Playwright test', async ({ page }) => {
+    await page.goto('https://google.com')
+    console.log(await page.title())
+    await expect(page).toHaveTitle('Google')
+})
