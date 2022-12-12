4e0a74f4a234879c93e957b7ce32bbcd853a3d72
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2022-12-12 16:32:48 +0500

    update pageFixture test case

--- tests/pageFixture.spec.js
+++ tests/pageFixture.spec.js
@@ -1,11 +1,9 @@
 const {test} = require('@playwright/test');
+const { use } = require('../playwright.config');
 
 //page --> used when the browser context is empty
 
-test('Browser fixture Playwright test',async ({page})=>{
-
-    const context = await browser.newContext(); 
-    const page = await context.newPage(); 
+test('Page fixture Playwright test',async ({page})=>{
 
     await page.goto('https://www.daraz.pk/')
     await this.page.waitForLoadState('load');
