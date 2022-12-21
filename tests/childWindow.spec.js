095c8872db71ed5996959f4e71be813b90b062ea
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2022-12-21 12:26:37 +0500

    add code for handling child windows.

--- tests/childWindow.spec.js
+++ tests/childWindow.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
-test('Handling Child windows',async({browser})=>{
+test('Handling tabs in playwright',async({browser})=>{
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
@@ -29,4 +29,24 @@ test('Handling Child windows',async({browser})=>{
     await pwd.type("learning")
     await page.waitForTimeout(10000)
 
+})
+
+test.only('Handling child windows in playwright', async({browser})=>{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        await page.locator('#openwindow').click()
+    ])
+
+    const text = await newPage.locator('div.text-center h2').first().textContent();
+    await page.waitForTimeout(2000)
+    console.log(text);
+
+    await page.bringToFront();
+    await page.locator('#name').type("Child Window")
+    await page.waitForTimeout(10000)
+    
 })
